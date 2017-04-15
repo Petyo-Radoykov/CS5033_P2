f8402f3e8a2cd57eb1f75d1d20807d05c421b4f4
Petyo Emilov Radoykov <petyoradoykov@gmail.com>
2017-04-15 23:18:46 +0100

    Sprint 1.1: server.js
    This file is used for starting the application. It is the controller of the applications.

---
+++ server.js
@@ -0,0 +1,63 @@
+// The CONTROLLER of the system
+
+/*#######################
+# IMPORT THE MODULES    #
+########################*/
+
+// External packges used
+////////////////////////
+var express    = require('express');        
+var app        = express();                 
+var bodyParser = require('body-parser');
+var mongoose   = require('mongoose');
+
+// Modules and files developed for the specific software system.
+////////////////////////////////////////////////////////////////
+// The configuration file.
+var config = require('./config'); 
+
+// The REST API
+var router_welcome = require('./routes/welcome');
+var router_users = require('./routes/user');
+var router_user_id = require('./routes/user_id');
+var router_booking = require('./routes/booking');
+var router_booking_id = require('./routes/booking_id');
+var router_user_id_booking_id = require('./routes/user_id_booking_id');
+
+
+
+
+/*#######################
+# SET THE SERVER        #
+########################*/
+// Connects to the database.
+mongoose.connect(config.database);
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Setting the post to 8080
+var port = process.env.PORT || 8080;        
+
+
+
+/*#######################
+# REGISTER THE REST API #
+########################*/		
+// all of our routes will be prefixed with /api
+app.use('/api', router_welcome);
+app.use('/api', router_users);
+app.use('/api', router_user_id);
+app.use('/api', router_booking);
+app.use('/api', router_booking_id);
+app.use('/api', router_user_id_booking_id);
+
+/*##################
+# START THE SERVER #
+##################*/
+app.listen(port);
+console.log('The server could be accessed at http://localhost:' + port);
+
+
